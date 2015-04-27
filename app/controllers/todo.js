11f5e212e809f3461d3776d30df68d09af877b2e
Jan Nonnen <jan.nonnen@gmx.net>
2015-04-27 19:38:43 +0200

    allow removing todos

--- app/controllers/todo.js
+++ app/controllers/todo.js
@@ -1,10 +1,28 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  editTodo: function(){
-    this.set('isEditing',true);
+  actions: {
+    editTodo: function(){
+      this.set('isEditing',true);
+    },
+    acceptChanges: function() {
+      this.model.set('isEditing', false);
+      if (Ember.isEmpty(this.model.get('title'))) {
+        this.send('removeTodo');
+      } else {
+        this.get('model').save();
+      }
+    },
+    removeTodo: function(){
+      var todo=this.get('model');
+      todo.deleteRecord();
+      todo.save();
+    }
+
+
   },
   isEditing: false,
+
   isCompleted: function(key, value) {
     var model = this.get('model');
     if (value === undefined) {
