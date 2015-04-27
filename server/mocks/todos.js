1d041d1ce92382e821aa242d91987c1a275b71e6
Jan Nonnen <jan.nonnen@gmx.net>
2015-04-27 18:24:35 +0200

    http-mock capability for todos
    ember g http-mock  todos

---
+++ server/mocks/todos.js
@@ -0,0 +1,36 @@
+module.exports = function(app) {
+  var express = require('express');
+  var todosRouter = express.Router();
+
+  todosRouter.get('/', function(req, res) {
+    res.send({
+      'todos': []
+    });
+  });
+
+  todosRouter.post('/', function(req, res) {
+    res.status(201).end();
+  });
+
+  todosRouter.get('/:id', function(req, res) {
+    res.send({
+      'todos': {
+        id: req.params.id
+      }
+    });
+  });
+
+  todosRouter.put('/:id', function(req, res) {
+    res.send({
+      'todos': {
+        id: req.params.id
+      }
+    });
+  });
+
+  todosRouter.delete('/:id', function(req, res) {
+    res.status(204).end();
+  });
+
+  app.use('/api/todos', todosRouter);
+};
