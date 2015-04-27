7122b15d75d03fc3f82993f78fec9445950b03e9
Jan Nonnen <jan.nonnen@gmx.net>
2015-04-27 18:41:35 +0200

    createTodo action

--- app/controllers/todos.js
+++ app/controllers/todos.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  actions: {
+    createTodo: function() {
+        var title = this.get('newTitle');
+        if (!title.trim()) { return; }
+        var todo = this.store.createRecord('todo', {
+            title: title,
+            isCompleted: false
+        });
+        this.set('newTitle', '');
+        todo.save();
+    }
+}
 });
